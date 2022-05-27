91b435709b21f8b26e4d47ff869379598343c912
Josh Perry <joshperry@outlook.com>
2022-05-27 13:34:36 +0100

    Client: send and receive chat messages

--- client/src/main.lua
+++ client/src/main.lua
@@ -1,16 +1,26 @@
+local utf8 = require("utf8")
+
 local batteries = require("lib.batteries")
 batteries:export()
 
+local flatbuffers = require("flatbuffers")
 local messageBuilders = require("messageBuilders")
+
+local MessageType = require("schemas.Message")
+local BaseMessage = require("schemas.BaseMessage")
+local ChatMessage = require("schemas.ChatMessage")
+local UpdatePositionMessage = require("schemas.UpdatePositionMessage")
 
 local socket = require("socket")
 local client
 
+local chatMessages = {}
 local player = {}
-local networked_entities = {}
 
 local lastHeartbeat = 0
 local heartbeatTimer = 0
+
+local messageToSend = ""
 
 local function generateUuid4()
     local _random = love.math.random
@@ -22,6 +32,20 @@ local function generateUuid4()
     end)
 
     return uuid4
+end
+
+local function handleMessage(messageBuffer)
+    local message = BaseMessage.GetRootAsBaseMessage(messageBuffer, 0)
+
+    if message:MessageType() == MessageType.ChatMessage then
+        local chatMessage = ChatMessage.New()
+        chatMessage:Init(message:Message().bytes, message:Message().pos)
+
+        table.insert(chatMessages, {
+            speaker = message:ClientId(),
+            message = chatMessage:Contents()
+        })
+    end
 end
 
 function love.load()
@@ -46,6 +70,9 @@ function love.update(dt)
 
         if data then
             lastHeartbeat = love.timer.getTime()
+
+            local buffer = flatbuffers.binaryArray.New(data)
+            handleMessage(buffer)
         end
     until not data
 
@@ -77,6 +104,10 @@ function love.update(dt)
     end
 end
 
+function love.textinput(text)
+    messageToSend = messageToSend..text
+end
+
 function love.draw()
     love.graphics.setColor(1, 1, 1)
     love.graphics.print(player.name, 10, 10)
@@ -90,15 +121,33 @@ function love.draw()
     love.graphics.rectangle("fill", math.round(player.x), math.round(player.y), player.w, player.h)
 
     love.graphics.setColor(1, 1, 1)
-    for _, e in ipairs(networked_entities) do
-        love.graphics.rectangle("line", e.x, e.y, e.w, e.h)
+
+    local lineHeight = 24
+    local showLastMessagesCount = 5
+    for i = showLastMessagesCount, 1, -1 do
+        local m = chatMessages[#chatMessages-showLastMessagesCount+i]
+
+        if m then
+            local y = love.graphics.getHeight() - (showLastMessagesCount - i + 2) * lineHeight
+            love.graphics.print(string.format("%s:\t%s", m.speaker, m.message), 10, y)
+        end
     end
+    love.graphics.print("> "..messageToSend, 10, love.graphics.getHeight() - lineHeight)
 end
 
 function love.keypressed(key)
     if key == "space" then
         client:send(messageBuilders.setName(player.clientId, "my new name"))
     elseif key == "return" then
-        client:send(messageBuilders.chat(player.clientId, "hello world"))
+        if #messageToSend > 0 then
+            client:send(messageBuilders.chat(player.clientId, messageToSend))
+            messageToSend = ""
+        end
+    elseif key == "backspace" then
+        local byteOffset = utf8.offset(messageToSend, -1)
+
+        if byteOffset then
+            messageToSend = string.sub(messageToSend, 1, byteOffset - 1)
+        end
     end
 end
