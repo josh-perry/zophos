42d52914b846e65ac3735c1d8f11edc9679fc776
Josh Perry <joshperry@outlook.com>
2022-05-29 19:02:15 +0100

    Server: add migration for X, Y columns on player
    This obviously isn't the ideal place for this: need to think more about how to store this kind of player data.

--- server/src/Zophos.Server/Zophos.Data/Migrations/20220529175929_AddPlayerXY.Designer.cs
+++ server/src/Zophos.Server/Zophos.Data/Migrations/20220529175929_AddPlayerXY.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Zophos.Data.Db;
 
@@ -10,9 +11,10 @@ using Zophos.Data.Db;
 namespace Zophos.Data.Migrations
 {
     [DbContext(typeof(GameServerDbContext))]
-    partial class GameServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220529175929_AddPlayerXY")]
+    partial class AddPlayerXY
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.5");
---
+++ server/src/Zophos.Server/Zophos.Data/Migrations/20220529175929_AddPlayerXY.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zophos.Data.Migrations
+{
+    public partial class AddPlayerXY : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "X",
+                table: "Players",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Y",
+                table: "Players",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "X",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Y",
+                table: "Players");
+        }
+    }
+}
