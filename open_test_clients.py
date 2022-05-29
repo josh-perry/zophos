4ab213eb5b44ab3c2b3556c3218b126107bb0dd0
Josh Perry <joshperry@outlook.com>
2022-05-29 18:38:50 +0100

    Misc: open_test_clients passes a name into the client
    Also only open 2 instead of 5!

--- open_test_clients.py
+++ open_test_clients.py
@@ -1,10 +1,11 @@
 import subprocess
 import time
 
-def open_client():
-    subprocess.Popen(["love", "client/src"])
+def open_client(i):
+    name = f"Player_{i}"
+    subprocess.Popen(["love", "client/src", f"name={name}"])
 
 if __name__ == "__main__":
-    for i in range(0, 5):
-        open_client()
+    for i in range(0, 2):
+        open_client(i)
         time.sleep(1)
